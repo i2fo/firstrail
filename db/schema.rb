82e3c5f9bc1b5983cc287cd0ca60db37dc99c3f0
Timur Glushan <timur.glushan@p-product.com>
2012-12-14 20:14:27 +0200

    user validation and password added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121214172939) do
+ActiveRecord::Schema.define(:version => 20121214174959) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(:version => 20121214172939) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
